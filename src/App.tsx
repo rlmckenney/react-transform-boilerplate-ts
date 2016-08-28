07cf70540d78d609c8f4cb79c391325f1decd15b
Robert McKenney <robert@bidfoundry.com>
2016-08-27 22:21:28 -0400

    Updated all dependencies to latest versions and added material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import colors from './colors';
 export default class App extends React.Component<{}, {}> {
 	render() {
 		return (
-			<div>
+			<div style={{margin: '40px 70px'}}>
 				<Counter increment={1} color={colors.NICE} />
 				<Counter increment={5} color={colors.SUPER_NICE} />
 			</div>
